b1bfdc10eb82c9e78d3a78b7fe7a1387717c15b1
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-14 17:40:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,6 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  #binding.pry
   # write your code here
 end
