27d499d99ea9865868248357a4c3d8c72fc39c55
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-14 17:37:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,8 @@ kickstarter = Nokogiri::HTML(html)
 
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
   # write your code here
 end
